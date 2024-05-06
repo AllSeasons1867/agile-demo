e8090cfec640dd939b740de2ee8f828132a72575
FW <35238548+AllSeasons1867@users.noreply.github.com>
2024-05-06 12:24:10 -0600

    final commit

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -1,8 +1,13 @@
 class SayController < ApplicationController
   def hello
     @time = Time.now
+    def say_hello
+      return "This is a hello call"
+    end 
+    @hello = say_hello()
   end
 
   def goodbye
+    @files = Dir.glob('*')
   end
 end
